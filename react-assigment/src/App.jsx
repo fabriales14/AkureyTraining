cf15580b29e1f1e72038573de11f69ac23d9b027
Fabricio Alvarado <fabrialvarado@AK-Guests-MacBook-Pro.local>
2019-05-09 12:05:06 -0600

    React app created

---
+++ react-assigment/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.scss';
+import logo from './assets/images/logo.png'
+
+class App extends React.Component {
+  render() {
+    return (
+      <header className="header">
+        <img className="header__logo" alt="" src={logo}></img>
+        <a className="header__icon"><i className="fas fa-bars fa-3x"></i></a>
+        <nav className="header__nav">
+          <ul className="header__nav__elements">
+            <li><a href="#">Directorio de Agentes</a></li>
+            <li><a href="#">Contacto</a></li>
+            <li><a href="#">Ingresar</a></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  } 
+}
+
+export default App;
