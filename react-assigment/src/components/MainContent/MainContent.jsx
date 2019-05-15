123b7a78c095e72a7308215ec2abc4f8fc906fec
Fabricio Alvarado <fabrialvarado@AK-Guests-MacBook-Pro.local>
2019-05-15 16:45:52 -0600

    Services changes, main component fixed

--- react-assigment/src/components/MainContent/MainContent.jsx
+++ react-assigment/src/components/MainContent/MainContent.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import './MainContent.scss';
-
-import Companies from '../../services/companies';
+import CompanyService  from '../../services/companies';
+import certified from '../../assets/images/certified.png';
+import agentImg from '../../assets/images/chuck.jpg';
 
 class MainContent extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      filter: ''
+      companies: [],
+      filter: '',
     }
+
+    this.getCompanies = this.getCompanies.bind(this);
+  }
+
+  componentWillMount() {
+    this.getCompanies();
   }
 
   handleChange(e) {
@@ -23,7 +31,33 @@ class MainContent extends React.Component {
     }
   }
 
+  /**
+   * This function gets all companies from the service
+   */
+  getCompanies() {
+    CompanyService.getCompanies().then( (response) => {
+      this.setState({ companies: response.data.companies})
+    });
+  }
+
   render() {
+    const companies = this.state.companies.filter(company => company.name.toLowerCase().includes(this.state.filter)).map ( company => (
+      <article className="container__body__item">
+          <div className="container__body__item__header">
+              <h1 className="container__body__item__header__title">{company.name}</h1>
+              { company.isCertified ? <img className="container__body__item__header__logo" src={certified}/> : null }
+          </div>
+          <div className="container__body__item__line"></div>
+          <section className="container__body__item__section">
+              <img className="container__body__item__section__img" src={agentImg}/>
+              <p className="container__body__item__section__info">{company.description}</p>
+          </section>
+          <footer className="container__body__item__footer">
+              <p className="container__body__item__footer__tittle">Desde ${company.rate} {company.hours}/hrs</p>
+              <button className="container__body__item__footer__button">Contratar</button>
+          </footer>
+      </article>
+    ))
     return (
       <div>
         <div className="search">
@@ -35,7 +69,9 @@ class MainContent extends React.Component {
             <input className="container__header__input" type="search" placeholder="Buscar por Ubicación" onChange={(e) => {this.handleChange(e)}} />
             <button className="container__header__button"/>
           </div>
-          <Companies filter={this.state.filter}/>
+          <div className="container__body" id="box">
+                { companies }
+            </div>
         </div>
       </div>
     );
--- react-assigment/src/services/companies.js
+++ react-assigment/src/services/companies.js
@@ -1,54 +1,17 @@
-import React from 'react';
-import certified from '../assets/images/certified.png';
-import agentImg from '../assets/images/chuck.jpg';
+import axios from 'axios';
 
-class Companies extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            companies: [],
-        };
-    }
+class CompanyService {
 
-    componentDidMount() {
-        fetch('https://api.myjson.com/bins/uptto')
-        .then( res => res.json())
-        .then( (data) => {
-            this.setState({
-                companies: data.companies
-            });
-        },
-        (error) => {
-            this.setState({
-                error
-            });
-        })
-    }
 
-    render() {
-        const companies = this.state.companies.filter(company => company.name.toLowerCase().includes(this.props.filter)).map ( company => (
-            <article className="container__body__item">
-                <div className="container__body__item__header">
-                    <h1 className="container__body__item__header__title">{company.name}</h1>
-                    <img className="container__body__item__header__logo" src={certified}/>
-                </div>
-                <div className="container__body__item__line"></div>
-                <section className="container__body__item__section">
-                    <img className="container__body__item__section__img" src={agentImg}/>
-                    <p className="container__body__item__section__info">{company.description}</p>
-                </section>
-                <footer className="container__body__item__footer">
-                    <p className="container__body__item__footer__tittle">Desde ${company.rate} {company.hours}/hrs</p>
-                    <button className="container__body__item__footer__button">Contratar</button>
-                </footer>
-            </article>
-        ))
-        return (
-            <div className="container__body" id="box">
-                { companies }
-            </div>
-        )
+    /**
+     *  Returns a axios promise of get request from API
+     */
+    static getCompanies() {
+        return axios.get('https://api.myjson.com/bins/uptto');
     }
 }
 
-export default Companies
+export default CompanyService;
+
+
+
